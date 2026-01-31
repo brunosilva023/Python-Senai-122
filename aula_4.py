4eb3548c748984b20905a8ad493f3d5967a03a97
Bruno Silva <brunodasilva271@gmail.com>
2026-01-31 15:50:23 -0300

    Add files via upload

---
+++ aula_4.py
@@ -0,0 +1,31 @@
+#ATIVIDADES:
+#1 - Crie um programa para efetuar a leitura de um número inteiro e apresentar o resultado do quadrado deste número.
+print('exercicio 1')
+numero_teste = int(input('informe um número:'))
+print(' a exponenciação deste número é: ',numero_teste**2)
+print()
+#2 - Crie duas variáveis para armazenar seu primeiro nome e sobrenome. Em seguida, concatene-as para formar seu nome completo e exiba o resultado.
+print('exercicio 2')
+nome = str(input('informe seu nome:'))
+sobrenome = str(input('informe seu sobrenome: '))
+print()
+print('seja bem vindo !',nome,sobrenome)
+print()
+#3 - Peça ao usuário para digitar dois números inteiros e armazene-os em variáveis. Realize a concatenação desses números como strings e exiba o resultado.
+print('exercicio 3')
+numero_1 = int(input('informe um número:'))
+numero_2 = int(input('informe um número:'))
+print('os números digitados foram : ',numero_1,' e ', numero_2)
+print()
+#4 - Crie uma variável para armazenar a palavra "Python". Em seguida, adicione um número inteiro ao final da palavra usando a concatenação e exiba o resultado.
+print('exercicio 4')
+palavra = 'Python'
+numero = int(input('informe um número:'))
+print(palavra,numero)
+print()
+#5 - Declare uma variável contendo uma frase. Em seguida, peça ao usuário para digitar uma palavra e concatene essa palavra no final da frase. Exiba o resultado. 
+print('exercicio 5')
+palavra = str(input('digite uma palavra: '))
+print('a palavra digitada foi : ',palavra)
+print()
+print('fim')
