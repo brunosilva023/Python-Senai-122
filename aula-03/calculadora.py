6dce11c8e9b69b68558b41630e64afc6e117fc30
Bruno Silva <brunodasilva271@gmail.com>
2026-01-31 14:43:56 -0300

    Add files via upload

---
+++ aula-03/calculadora.py
@@ -0,0 +1,15 @@
+print('calculadora')
+num_1 = float(input('Digite um número: '))
+num_2 = float(input('Digite um número: '))
+  
+print()
+soma = num_1 + num_2
+subtracao = num_1 - num_2
+multiplicacao = num_1 * num_2
+divisao = num_1 / num_2
+
+print('a soma dos números é : ' , soma)
+print('a subtração dos números é : ' , subtracao)
+print('a multiplicação dos números é : ' , multiplicacao)
+print('a divisão dos números é : ' , divisao)
+
