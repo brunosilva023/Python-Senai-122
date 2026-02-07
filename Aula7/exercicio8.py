329d9ca409b24691c51ce6aa86c60da9341dada4
Bruno Silva <brunodasilva271@gmail.com>
2026-02-07 15:48:29 -0300

    Add files via upload

---
+++ Aula7/exercicio8.py
@@ -0,0 +1,51 @@
+print('Aula - 8')
+
+print('1 Peça para o usuário digitar um número, verifique se um número é positivo, negativo ou zero.')
+numero = int(input('Digite um numero:'))
+if numero >0:
+    print('o numero digitado foi :', numero, 'Então ele é positivo')
+elif numero == 0:
+    print('o numero digitado foi :', numero, 'Então ele é zero')
+else:
+    print('o numero digitado foi :', numero, 'Então ele é negativo')
+
+print('\n2 Peça para o usuário digitar a idade, verifique se uma pessoa pode votar com base na idade.')
+idade = int(input('Digite sua idade:'))
+if idade >= 18:
+    print('a idade informada foi :', idade, 'Apto a votar')
+else:
+    print('a idade informada foi :', idade, 'não está apto a votar')
+
+print('\n3 Declara uma variável com um número qualquer, determine se um número é par ou ímpar.')
+numero = int(input('Digite um numero:'))
+resto = numero % 2 
+if resto==0:
+    print('o numero informado  foi :', numero, 'então ele é par')
+else:
+    print('o numero informado  foi :', numero, 'então ele é impar')
+
+
+print('\n4 Usuário vai digitar 3  números, para criar um triângulo, verifique se um triângulo é equilátero, isósceles ou escaleno')
+medida1 = int(input('Digite a primeira medida:'))
+medida2 = int(input('Digite a segunda medida:'))
+medida3 = int(input('Digite a terceira medida:'))
+
+if medida1== medida2 or medida3:
+    print('é um triangulo equilatero')
+
+
+# Um triângulo é chamado de equilátero se todos os lados possuem a mesma medida. 
+# Um triângulo é chamado de isósceles se dois lados possuem a mesma medida. 
+# Um triângulo é chamado de escaleno se todos os lados possuem medidas diferentes.
+
+# 5*
+
+	# Determine se um número é múltiplo de 5 e 7.
+
+# 6*
+
+# Verifique se um número é positivo e maior que 10
+
+# 7*
+
+# Verifique se um número é divisível por 3 ou 5.
