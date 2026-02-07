1ee4144062e243d8134e4ad0360ff0ec219a421f
Bruno Silva <brunodasilva271@gmail.com>
2026-02-07 16:59:16 -0300

    Add files via upload

--- Aula7/exercicio8.py
+++ Aula7/exercicio8.py
@@ -9,12 +9,16 @@ elif numero == 0:
 else:
     print('o numero digitado foi :', numero, 'Então ele é negativo')
 
+print('------'*20)
+
 print('\n2 Peça para o usuário digitar a idade, verifique se uma pessoa pode votar com base na idade.')
 idade = int(input('Digite sua idade:'))
 if idade >= 18:
     print('a idade informada foi :', idade, 'Apto a votar')
 else:
     print('a idade informada foi :', idade, 'não está apto a votar')
+
+print('------'*20)
 
 print('\n3 Declara uma variável com um número qualquer, determine se um número é par ou ímpar.')
 numero = int(input('Digite um numero:'))
@@ -24,6 +28,7 @@ if resto==0:
 else:
     print('o numero informado  foi :', numero, 'então ele é impar')
 
+print('------'*20)
 
 print('\n4 Usuário vai digitar 3  números, para criar um triângulo, verifique se um triângulo é equilátero, isósceles ou escaleno')
 medida1 = int(input('Digite a primeira medida:'))
@@ -37,6 +42,8 @@ if medida1 != medida2 != medida3!= medida1:
 else:
     print('é um triangulo isosceles, pois todos os lados possuem medidas diferentes')
 
+print('------'*20)
+
 print('\n5 Determine se um número é múltiplo de 5 e 7.')
 numero = int(input('Digite um numero:'))
 resto5 = numero%5
@@ -49,6 +56,8 @@ elif resto7==0:
 else:
     print('o numero informado  foi :', numero, 'então ele não é multiplo de 5 e 7')
 
+print('------'*20)
+
 print('\n6 Verifique se um número é positivo e maior que 10')
 numero = int(input('Digite um numero:'))
 if numero >=0 and numero>10:
@@ -57,6 +66,8 @@ elif numero>=0 and numero<=10:
     print('o numero digitado foi :', numero, 'Então ele é  positivo,maior que zero e menor ou igual que 10')
 else: 
     print('o numero digitado foi :', numero, 'Então ele é negativo')
+
+print('------'*20)
 
 print('\n7 Verifique se um número é divisível por 3 ou 5.')
 numero = int(input('Digite um numero:'))
@@ -69,3 +80,6 @@ elif resto5==0:
     print('o numero informado  foi :', numero, 'então ele é é divisivel de 5')
 else:
     print('o numero informado  foi :', numero, 'então ele não é divisivel de 3 e 5')
+
+print('------'*20)
+print('exercicios finalizados com sucesso !')
